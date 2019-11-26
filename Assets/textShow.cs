e178ba4ed7da1cbd58dabd732747833f96a786a6
Mconkla <lackinggumption@web.de>
2019-11-26 14:26:23 +0100

    So much changes
    a lot new changes

---
+++ Assets/textShow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textShow : MonoBehaviour
+{
+    public attackSystem mySystem;
+    public Text attackText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attackText.text = mySystem.myText;
+        
+    }
+}
